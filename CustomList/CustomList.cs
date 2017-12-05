eb94564b3285597de3847c4fa47d9d4b17be665c
RickK213 <rkippert@att.net>
2017-12-05 11:47:10 -0600

    Tests for Add method complete. Add method functioning. All Add tests pass

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    public class CustomList<T>
+    {
+        //member variables
+        int count;
+        T[] listItems;
+
+        //properties
+        public int Count
+        {
+            get { return count; }
+        }
+
+        //The below is an indexer================================================================
+        //Notes on indexers:
+        //An indexer is a class property
+        //It is a 'parameterized' property  - it has parameters
+        //'ref' and 'out' paramter modifiers are now allowed
+        //at least one parameter should be specified
+
+        public T this [int i]
+        {
+            get
+            {
+                return listItems[i];
+            }
+            set
+            {
+                listItems[i] = value;
+            }
+        }
+        //End of indexer==========================================================================
+
+        //constructor
+        public CustomList()
+        {
+            count = 0;
+
+        }
+
+        //member methods
+
+        //Add to end of array
+        public void Add(T item)
+        {
+            T[] newListItems = new T[count+1];
+            newListItems[count] = item;
+            listItems = newListItems;
+            count++;
+        }
+
+        //Remove first instance from array
+        public void Remove()
+        {
+
+        }
+
+    }
+}
---
+++ CustomListTesting/CustomListTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClass;
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class CustomListTest
+    {
+
+        [TestMethod]
+        public void Add_AddFirstString_FirstItemEqualsStringAdded()
+        {
+            //When I call the Add method and pass in a string, I expect the first item in the CustomList to be equal to the string I added
+
+            //arrange
+            string testString = "Rick";
+            CustomList<string> myList = new CustomList<string>();
+
+            //act
+            myList.Add(testString);
+
+            //assert
+            //How to reference an index of a custom class? Refer to the internal array.
+            //answer: LOOK UP C# indexer!!
+            //also, will need to use IEnumerable interface later on!!!
+            Assert.AreEqual(myList[0], testString);
+        }
+
+        [TestMethod]
+        public void Add_AddFirstItem_CountEqualsOne()
+        {
+            //When I call the Add method for the first time, I expect the count property of the CustomList object to be equal to 1
+
+            //arrange
+            string testString = "Rick";
+            CustomList<string> myList = new CustomList<string>();
+
+            //act
+            myList.Add(testString);
+
+            //assert
+            Assert.AreEqual(myList.Count, 1); //Need to set up Count as a property!!! Read-only!
+        }
+
+        [TestMethod]
+        public void Add_AddFiveItems_CountEqualsFive()
+        {
+            //When I call the Add method five times, I expect the count property of the CustomList object to be equal to 5
+
+            //arrange
+            string testString = "Rick";
+            CustomList<string> myList = new CustomList<string>();
+
+            //act
+            for (int i=0; i<5; i++ )
+            {
+                myList.Add(testString);
+            }
+
+            //assert
+            Assert.AreEqual(myList.Count, 5); //Need to set up Count as a property!!!
+        }
+
+        [TestMethod]
+        public void Add_AddFirstObject_FirstItemEqualsObjectAdded()
+        {
+            //When I call the Add method and pass in an object, I expect the first item in the CustomList to be equal to the object I added
+
+            //arrange
+            Object obj = new Object();
+            CustomList<Object> myList = new CustomList<Object>();
+
+            //act
+            myList.Add(obj);
+
+            //assert
+            Assert.AreEqual(myList[0], obj);
+        }
+
+        [TestMethod]
+        public void Add_AddNullValueType_FirstItemEqualsNull()
+        {
+            //When I call the Add method and pass in an null value type, I expect the first item in the CustomList to be equal to null
+
+            //arrange
+            int? number = null;
+            CustomList<int?> myList = new CustomList<int?>();
+
+            //act
+            myList.Add(number);
+
+            //assert
+            Assert.IsNull(myList[0]);
+        }
+
+        [TestMethod]
+        public void Add_AddThreeItems_LastItemInListIsLastOneAdded()
+        {
+            //When I add three items to the custom list, I expect the last item added to be equal to the last index in the list
+
+            //arrange
+            string itemOne = "Item1";
+            string itemTwo = "Item2";
+            string itemThree = "Item3";
+            CustomList<string> myList = new CustomList<string>();
+
+            //act
+            myList.Add(itemOne);
+            myList.Add(itemTwo);
+            myList.Add(itemThree);
+
+            //assert
+            Assert.AreEqual(myList[2], itemThree);
+        }
+
+
+    }
+}
